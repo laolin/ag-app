5fad6d137f69b7641b7554318f6e15286d58d712
LaoLin <hi@laolin.com>
2014-03-31 22:44:52 +0800

    不同区块分成不同的controller

--- wave/js/index.js
+++ wave/js/index.js
@@ -2,14 +2,21 @@
 ;
 var waveApp = angular.module('waveApp', ['ui.bootstrap']);
 
+//A.1 通用数据
 waveApp.data={};
 waveApp.data.styles=['blue','green','red','yellow',
   'orange','pink','purple','lime','magenta','teal'];//,'white','black'
 
 
-waveApp.controller('waveCtrl', function ($scope) {
+//C.0 AppCtrl controller
+waveApp.controller('AppCtrl', function ($scope) {
   //DEBUG标记，正式页面应设为false
   $scope.debug=false;
+});
+
+//C.1 TopNavCtrl controller
+waveApp.controller("TopNavCtrl", function ($scope) {
+
   //页面左上角，APP名称
   $scope.appName = "LaoLin地震波助手";
   //导航菜单项目
@@ -33,6 +40,10 @@ waveApp.controller('waveCtrl', function ($scope) {
     {text:'如何下载AutoCAD格式的地震波',link:'#/index7'},
     {text:'如何下载AutoCAD格式的反应谱',link:'#/index8'}
   ];
+});
+
+//C.3 MarketingCtrl controller
+waveApp.controller("MarketingCtrl", function ($scope) {
   var row1=[
     {width:4,imgSize:200,link:'#/about',img:'http://files.laolin.com/2013/structural-engineering/linjp-2012.9.3-180x180.jpg',title:'个人简历',text:'林建萍，高级工程师，国家一级注册结构工程师'},
     {width:4,imgSize:200,link:'#/about',img:'http://files.laolin.com/2013/development/20130715-development.jpg',title:'老林编程',text:'编程是老林的业余爱好。'},
@@ -44,11 +55,12 @@ waveApp.controller('waveCtrl', function ($scope) {
     {width:3,imgSize:150,link:'#/about',img:'http://files.laolin.com/2013/laolin-family/20130525.laolin.family-200.jpg',title:'Title -3-3-1',text:'3333333333test text 1 11 测试'},
     {width:3,imgSize:150,link:'#/about',img:'http://files.laolin.com/2013/structural-engineering/linjp-2012.9.3-180x180.jpg',title:'Ti4444444tle -2-4-1',text:'444444444test 测试text 测试 33333 测试'}
    ];
-  
   $scope.rows=[row1,row2];
 });
-waveApp.controller('CarouselDemoCtrl',CarouselDemoCtrl);
-function CarouselDemoCtrl($scope) {
+
+
+//C.2 CarouselDemoCtrl controller
+waveApp.controller('CarouselDemoCtrl',function ($scope) {
   $scope.myInterval = 5000;
   var slides = $scope.slides = [];
   $scope.addSlide = function() {
@@ -61,7 +73,7 @@ function CarouselDemoCtrl($scope) {
   for (var i=0; i<4; i++) {
     $scope.addSlide();
   }
-}
+});
 
 
 
