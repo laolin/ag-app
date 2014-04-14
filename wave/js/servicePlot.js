7c18b12dfd1baf63850210c2b4d3ae7636c492be
LaoLin <hi@laolin.com>
2014-04-14 22:25:50 +0800

    servicePlot目前没有用了，把肯定不会再用的先删了

--- wave/js/servicePlot.js
+++ wave/js/servicePlot.js
@@ -1,24 +1,5 @@
 // 使用jqPlot
 LaolinApp.service('servicePlot',["$http","$log",function ($http,$log) {
-  var chartTpl="comm-plot-area.html";
-  var chartHtml="";
-  function fetchChart() {
-    $log.log("fetchChart");
-    return $http.get(chartTpl).success(function(data){
-      chartHtml=data;
-      $log.log("fetchChart DONE.");
-    })
-  }
-  this.loadChart=function(selc) {
-    if(chartHtml) {
-      $log.log("reuse chart");
-      $(selc).html(chartHtml);
-    } else {
-      fetchChart().then(function(data){
-        $(selc).html(chartHtml);
-      });
-    }
-  }
   
   ///TODO: 支持多条曲线
   this.plotData=function(d,label){
